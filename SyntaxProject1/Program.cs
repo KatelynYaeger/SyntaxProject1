c367ce1c1c6cc7217a1b6df444e8daebdbe39813
Katelyn Yaeger <knyaeger93@gmail.com>
2022-09-12 16:22:40 -0500

    final commit syntax

---
+++ SyntaxProject1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SyntaxProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //inferred typing, string interpolation, and ternary operator
+
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //   response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //  response = answer + “greater than or equal to nine”;
+            //}
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+
+        }
+    }
+}
+
